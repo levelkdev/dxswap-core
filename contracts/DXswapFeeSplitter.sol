8dff1650d0f35ec6874f9c8f4caf635fe04c622f
karczu <oskar.karcz@rumblefish.pl>
2022-09-01 12:12:22 +0200

    refactor: remove isContract & pairFor internal functions and replace by calling factory

--- contracts/DXswapFeeSplitter.sol
+++ contracts/DXswapFeeSplitter.sol
@@ -1,9 +1,9 @@
 pragma solidity =0.8.15;
 
-import './interfaces/IDXswapFactory.sol';
-import './interfaces/IDXswapPair.sol';
-import './interfaces/IWETH.sol';
-import './libraries/TransferHelperV2.sol';
+import "./interfaces/IDXswapFactory.sol";
+import "./interfaces/IDXswapPair.sol";
+import "./interfaces/IWETH.sol";
+import "./libraries/TransferHelperV2.sol";
 
 contract DXswapFeeSplitter {
     address public owner;
@@ -22,7 +22,11 @@ contract DXswapFeeSplitter {
 
     mapping(address => ExternalFeeReceiver) public externalFeeReceivers;
 
-    event TakeProtocolFee(address indexed sender, address indexed to, uint256 NumberOfPairs);
+    event TakeProtocolFee(
+        address indexed sender,
+        address indexed to,
+        uint256 NumberOfPairs
+    );
 
     constructor(
         address _owner,
@@ -42,62 +46,51 @@ contract DXswapFeeSplitter {
 
     // called by the owner to set the new owner
     function transferOwnership(address newOwner) external {
-        require(msg.sender == owner, 'DXswapFeeSplitter: FORBIDDEN');
+        require(msg.sender == owner, "DXswapFeeSplitter: FORBIDDEN");
         owner = newOwner;
     }
 
     // called by the owner to change receivers addresses
-    function changeReceivers(address _ethReceiver, address _fallbackReceiver) external {
-        require(msg.sender == owner, 'DXswapFeeSplitter: FORBIDDEN');
+    function changeReceivers(address _ethReceiver, address _fallbackReceiver)
+        external
+    {
+        require(msg.sender == owner, "DXswapFeeSplitter: FORBIDDEN");
         ethReceiver = _ethReceiver;
         fallbackReceiver = _fallbackReceiver;
     }
 
     // Returns sorted token addresses, used to handle return values from pairs sorted in this order
-    function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
-        require(tokenA != tokenB, 'DXswapFeeSplitter: IDENTICAL_ADDRESSES');
-        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        require(token0 != address(0), 'DXswapFeeSplitter: ZERO_ADDRESS');
-    }
-
-    // Helper function to know if an address is a contract, extcodesize returns the size of the code of a smart
-    //  contract in a specific address
-    function isContract(address addr) internal view returns (bool) {
-        uint256 size;
-        assembly {
-            size := extcodesize(addr)
-        }
-        return size > 0;
-    }
-
-    // Calculates the CREATE2 address for a pair without making any external calls
-    // Taken from DXswapLibrary, removed the factory parameter
-    function pairFor(address tokenA, address tokenB) internal view returns (address pair) {
-        (address token0, address token1) = sortTokens(tokenA, tokenB);
-        pair = address(
-            uint160(
-                uint256(
-                    keccak256(
-                        abi.encodePacked(
-                            hex'ff',
-                            factory,
-                            keccak256(abi.encodePacked(token0, token1)),
-                            hex'd306a548755b9295ee49cc729e13ca4a45e00199bbd890fa146da43a50571776' // init code hash
-                        )
-                    )
-                )
-            )
-        );
+    function sortTokens(address tokenA, address tokenB)
+        internal
+        pure
+        returns (address token0, address token1)
+    {
+        require(tokenA != tokenB, "DXswapFeeSplitter: IDENTICAL_ADDRESSES");
+        (token0, token1) = tokenA < tokenB
+            ? (tokenA, tokenB)
+            : (tokenB, tokenA);
+        require(token0 != address(0), "DXswapFeeSplitter: ZERO_ADDRESS");
     }
 
     // Done with code form DXswapRouter and DXswapLibrary, removed the deadline argument
-    function _swapTokensForETH(uint256 amountIn, address fromToken) internal returns (uint256 amountOut) {
-        IDXswapPair pairToUse = IDXswapPair(pairFor(fromToken, nativeCurrencyWrapper));
+    function _swapTokensForETH(uint256 amountIn, address fromToken)
+        internal
+        returns (uint256 amountOut)
+    {
+        IDXswapPair pairToUse = IDXswapPair(
+            factory.getPair(fromToken, nativeCurrencyWrapper)
+        );
 
         (uint256 reserve0, uint256 reserve1, ) = pairToUse.getReserves();
-        (uint256 reserveIn, uint256 reserveOut) = fromToken < nativeCurrencyWrapper ? (reserve0, reserve1) : (reserve1, reserve0);
+        (uint256 reserveIn, uint256 reserveOut) = fromToken <
+            nativeCurrencyWrapper
+            ? (reserve0, reserve1)
+            : (reserve1, reserve0);
 
-        require(reserveIn > 0 && reserveOut > 0, 'DXswapFeeSplitter: INSUFFICIENT_LIQUIDITY'); // should never happen since pool was checked before
+        require(
+            reserveIn > 0 && reserveOut > 0,
+            "DXswapFeeSplitter: INSUFFICIENT_LIQUIDITY"
+        ); // should never happen since pool was checked before
         uint256 amountInWithFee = amountIn * (10000 - pairToUse.swapFee());
         uint256 numerator = amountInWithFee * reserveOut;
         uint256 denominator = reserveIn * 10000 + amountInWithFee;
@@ -105,7 +98,10 @@ contract DXswapFeeSplitter {
 
         TransferHelper.safeTransfer(fromToken, address(pairToUse), amountIn);
 
-        (uint256 amount0Out, uint256 amount1Out) = fromToken < nativeCurrencyWrapper ? (uint256(0), amountOut) : (amountOut, uint256(0));
+        (uint256 amount0Out, uint256 amount1Out) = fromToken <
+            nativeCurrencyWrapper
+            ? (uint256(0), amountOut)
+            : (amountOut, uint256(0));
 
         pairToUse.swap(amount0Out, amount1Out, address(this), new bytes(0));
     }
@@ -116,14 +112,17 @@ contract DXswapFeeSplitter {
         address token1,
         uint256 amount
     ) internal view returns (bool) {
-        address pair = pairFor(token0, token1);
-        if (!isContract(pair)) return false;
+        address pair = factory.getPair(token0, token1);
+        if (pair == address(0)) return false;
 
-        (uint256 reserve0, uint256 reserve1, ) = IDXswapPair(pair).getReserves();
-        (uint256 reserveIn, uint256 reserveOut) = token0 < token1 ? (reserve0, reserve1) : (reserve1, reserve0);
+        (uint256 reserve0, uint256 reserve1, ) = IDXswapPair(pair)
+            .getReserves();
+        (uint256 reserveIn, uint256 reserveOut) = token0 < token1
+            ? (reserve0, reserve1)
+            : (reserve1, reserve0);
         if (reserveIn == 0 || reserveOut == 0) return false;
 
-        uint256 priceImpact = amount * 10000 / reserveIn; // simplified formula
+        uint256 priceImpact = (amount * 10000) / reserveIn; // simplified formula
         if (priceImpact > maxSwapPriceImpact) return false;
 
         return true;
@@ -136,19 +135,36 @@ contract DXswapFeeSplitter {
         address token,
         uint256 amount
     ) internal {
-        address _externalFeeReceiver = externalFeeReceivers[pair].externalReceiver;
-        uint16 _percentFeeToExternalReceiver = externalFeeReceivers[pair].feePercentage;
+        address _externalFeeReceiver = externalFeeReceivers[pair]
+            .externalReceiver;
+        uint16 _percentFeeToExternalReceiver = externalFeeReceivers[pair]
+            .feePercentage;
 
-        if (_percentFeeToExternalReceiver > 0 && _externalFeeReceiver != address(0)) {
-            uint256 feeToExternalReceiver = amount * _percentFeeToExternalReceiver / 10000;
+        if (
+            _percentFeeToExternalReceiver > 0 &&
+            _externalFeeReceiver != address(0)
+        ) {
+            uint256 feeToExternalReceiver = (amount *
+                _percentFeeToExternalReceiver) / 10000;
             uint256 feeToAvatarDAO = amount - feeToExternalReceiver;
             if (token == nativeCurrencyWrapper) {
                 IWETH(nativeCurrencyWrapper).withdraw(amount);
-                TransferHelper.safeTransferETH(_externalFeeReceiver, feeToExternalReceiver);
+                TransferHelper.safeTransferETH(
+                    _externalFeeReceiver,
+                    feeToExternalReceiver
+                );
                 TransferHelper.safeTransferETH(ethReceiver, feeToAvatarDAO);
             } else {
-                TransferHelper.safeTransfer(token, _externalFeeReceiver, feeToExternalReceiver);
-                TransferHelper.safeTransfer(token, fallbackReceiver, feeToAvatarDAO);
+                TransferHelper.safeTransfer(
+                    token,
+                    _externalFeeReceiver,
+                    feeToExternalReceiver
+                );
+                TransferHelper.safeTransfer(
+                    token,
+                    fallbackReceiver,
+                    feeToAvatarDAO
+                );
             }
         } else {
             if (token == nativeCurrencyWrapper) {
@@ -166,7 +182,10 @@ contract DXswapFeeSplitter {
         address token,
         uint256 amount
     ) internal {
-        if (token != nativeCurrencyWrapper && _isSwapPossible(token, nativeCurrencyWrapper, amount)) {
+        if (
+            token != nativeCurrencyWrapper &&
+            _isSwapPossible(token, nativeCurrencyWrapper, amount)
+        ) {
             // If it is not nativeCurrencyWrapper and there is a direct path to nativeCurrencyWrapper, swap tokens
             uint256 amountOut = _swapTokensForETH(amount, token);
             _splitAndTransferFee(pair, nativeCurrencyWrapper, amountOut);
@@ -181,30 +200,43 @@ contract DXswapFeeSplitter {
         for (uint256 i = 0; i < pairs.length; i++) {
             address token0 = pairs[i].token0();
             address token1 = pairs[i].token1();
-            pairs[i].transfer(address(pairs[i]), pairs[i].balanceOf(address(this)));
+            pairs[i].transfer(
+                address(pairs[i]),
+                pairs[i].balanceOf(address(this))
+            );
             (uint256 amount0, uint256 amount1) = pairs[i].burn(address(this));
-            if (amount0 > 0) _takeTokenOrETH(address(pairs[i]), token0, amount0);
-            if (amount1 > 0) _takeTokenOrETH(address(pairs[i]), token1, amount1);
+            if (amount0 > 0)
+                _takeTokenOrETH(address(pairs[i]), token0, amount0);
+            if (amount1 > 0)
+                _takeTokenOrETH(address(pairs[i]), token1, amount1);
         }
         emit TakeProtocolFee(msg.sender, ethReceiver, pairs.length);
     }
 
     // called by the owner to set maximum swap price impact allowed for single token-nativeCurrencyWrapper swap (within 0-100% range)
     function setMaxSwapPriceImpact(uint16 _maxSwapPriceImpact) external {
-        require(msg.sender == owner, 'DXswapFeeSplitter: CALLER_NOT_OWNER');
-        require(_maxSwapPriceImpact > 0 && _maxSwapPriceImpact < 10000, 'DXswapFeeSplitter: FORBIDDEN_PRICE_IMPACT');
+        require(msg.sender == owner, "DXswapFeeSplitter: CALLER_NOT_OWNER");
+        require(
+            _maxSwapPriceImpact > 0 && _maxSwapPriceImpact < 10000,
+            "DXswapFeeSplitter: FORBIDDEN_PRICE_IMPACT"
+        );
         maxSwapPriceImpact = _maxSwapPriceImpact;
     }
 
     // called by the owner to set external fee receiver address
-    function setExternalFeeReceiver(address _pair, address _externalReceiver) external {
-        require(msg.sender == owner, 'DXswapFeeSplitter: CALLER_NOT_OWNER');
+    function setExternalFeeReceiver(address _pair, address _externalReceiver)
+        external
+    {
+        require(msg.sender == owner, "DXswapFeeSplitter: CALLER_NOT_OWNER");
         externalFeeReceivers[_pair].externalReceiver = _externalReceiver;
     }
 
     // called by the owner to set fee percentage to external receiver
-    function setFeePercentageToExternalReceiver(address _pair, uint16 _feePercentageToExternalReceiver) external {
-        require(msg.sender == owner, 'DXswapFeeSplitter: CALLER_NOT_OWNER');
+    function setFeePercentageToExternalReceiver(
+        address _pair,
+        uint16 _feePercentageToExternalReceiver
+    ) external {
+        require(msg.sender == owner, "DXswapFeeSplitter: CALLER_NOT_OWNER");
         IDXswapPair swapPair = IDXswapPair(_pair);
         uint256 feeReceiverBalance = swapPair.balanceOf(address(this));
         if (feeReceiverBalance > 0) {
@@ -213,18 +245,25 @@ contract DXswapFeeSplitter {
             address token1 = swapPair.token1();
             swapPair.transfer(address(swapPair), feeReceiverBalance);
             (uint256 amount0, uint256 amount1) = swapPair.burn(address(this));
-            if (amount0 > 0) _takeTokenOrETH(address(swapPair), token0, amount0);
-            if (amount1 > 0) _takeTokenOrETH(address(swapPair), token1, amount1);
+            if (amount0 > 0)
+                _takeTokenOrETH(address(swapPair), token0, amount0);
+            if (amount1 > 0)
+                _takeTokenOrETH(address(swapPair), token1, amount1);
             emit TakeProtocolFee(msg.sender, ethReceiver, 1);
         }
-        require(swapPair.balanceOf(address(this)) == 0, 'DXswapFeeSplitter: TOKENS_NOT_BURNED');
+        require(
+            swapPair.balanceOf(address(this)) == 0,
+            "DXswapFeeSplitter: TOKENS_NOT_BURNED"
+        );
 
         // fee percentage check
         require(
-            _feePercentageToExternalReceiver >= 0 && _feePercentageToExternalReceiver <= 5000,
-            'DXswapFeeSplitter: FORBIDDEN_FEE_PERCENTAGE_SPLIT'
+            _feePercentageToExternalReceiver >= 0 &&
+                _feePercentageToExternalReceiver <= 5000,
+            "DXswapFeeSplitter: FORBIDDEN_FEE_PERCENTAGE_SPLIT"
         );
         // update the split percentage for specific pair
-        externalFeeReceivers[_pair].feePercentage = _feePercentageToExternalReceiver;
+        externalFeeReceivers[_pair]
+            .feePercentage = _feePercentageToExternalReceiver;
     }
 }
